984fe0bbce38e627751a8aef1333fdeba47b4f87
Jürgen Hahn <juergen.hahn@core-sensing.de>
2026-04-21 23:05:48 +0200

    refactor: remove unused useRef import in useImportProgress hook

--- frontend/src/hooks/useImportProgress.ts
+++ frontend/src/hooks/useImportProgress.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppStore } from '../store/useAppStore'
 import { apiFetch } from '../services/api'
 
